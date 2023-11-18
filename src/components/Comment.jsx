b4366d4ac2646dc3909ed502f860d4d4f09b13ae
ThaSMorato <tha.s.morato@gmail.com>
2023-11-18 15:12:20 -0300

    likes count and handling buttons

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,22 @@
+/* eslint-disable react/prop-types */
 
 import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import Avatar from './Avatar';
+import { useState } from 'react';
 
-function Comment({content}) {
+function Comment({content, onDeleteComment}) {
+
+  const [likeCount, setLikeCount] = useState(0)
+
+  const handleDeleteComment = () => {
+    onDeleteComment(content);
+  }
+
+  const handleLikeComment = () => {
+    setLikeCount((state) => state + 1)
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/ThaSMorato.png" />
@@ -16,7 +29,7 @@ function Comment({content}) {
               <time title="11 de Maio as 08:13h" dateTime="2022-05-11 08:13:30">Cerca de 1h atras</time>
             </div>
 
-            <button title='delete'>
+            <button onClick={handleDeleteComment} title='delete'>
               <Trash size={24} />
             </button>
           </header>
@@ -24,9 +37,9 @@ function Comment({content}) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{ likeCount }</span>
           </button>
         </footer>
       </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR'
@@ -10,9 +9,7 @@ import { useState } from 'react';
 
 function Post({author, publishedAt,  content }) {
   const [newCommentText, setNewCommentText] = useState('')
-  const [comments, setComments] = useState([
-      "Muito bom Devon, parabéns!! 👏👏",
-  ])
+  const [comments, setComments] = useState([])
 
   const publishedDateFormatter = format(publishedAt, "d 'de' LLLL 'as' HH:mm'h'", { locale: ptBR })
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
@@ -28,8 +25,20 @@ function Post({author, publishedAt,  content }) {
   }
 
   function newCommentChange(event) {
+    event.target.setCustomValidity('')
     setNewCommentText(event.target.value)
   }
+
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter(comment => comment !== commentToDelete)
+    setComments(commentsWithoutDeletedOne);
+  }
+
+  function handleNewCommentInvalid(event) {
+    event.target.setCustomValidity('Ésse campo eh obrigatorio')
+  }
+
+  const isNewCommentEmpty = newCommentText.length === 0
 
   return (
     <article className={styles.post}>
@@ -49,9 +58,9 @@ function Post({author, publishedAt,  content }) {
         {
           content.map((item) =>{
             if(item.type === 'paragraph') {
-              return <p>{item.content}</p>
+              return <p key={item.content}>{item.content}</p>
             } else if(item.type === 'link') {
-              return <p><a href='#'>{item.content}</a></p>
+              return <p key={item.content}><a href='#'>{item.content}</a></p>
             }
           })
         }
@@ -65,16 +74,27 @@ function Post({author, publishedAt,  content }) {
           placeholder="Deixe seu comentario"
           onChange={newCommentChange}
           value={newCommentText}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
         <footer>
-          <button onClick={handleCreateNewComment} >Publicar</button>
+          <button
+            disabled={isNewCommentEmpty}
+            onClick={handleCreateNewComment}
+          >
+            Publicar
+          </button>
         </footer>
       </form>
 
       <div className={styles.commentList}>
         {
           comments.map((comment) => (
-            <Comment content={comment} />
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
           ))
         }
       </div>
